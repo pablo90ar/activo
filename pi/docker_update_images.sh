2023cc89df87c789ca41a970f3c958ad4c1b9c2b
Pablo Funes <pabafunes@teco.com.ar>
2026-03-02 23:35:29 -0300

    includes sqlite install script

--- pi/docker_update_images.sh
+++ pi/docker_update_images.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker compose down
-docker pull --platform linux/arm64 pablo90ar/activo-backend:latest
+docker pull --platform linux/amd64 pablo90ar/activo-backend:latest
 docker pull --platform linux/amd64 pablo90ar/activo-frontend:latest
 docker compose up -d
