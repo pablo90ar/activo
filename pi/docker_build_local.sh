7f6a52add07c70458de9f18420215c9aa2ff1e23
Pablo Funes <pabafunes@teco.com.ar>
2026-03-03 11:08:24 -0300

    minor fix

--- pi/docker_build_local.sh
+++ pi/docker_build_local.sh
@@ -6,6 +6,8 @@ echo "Building backend..."
 cd back
 docker build -t pablo90ar/activo-backend:latest .
 
+cd ..
+
 echo "Building frontend..."
 cd front
 docker build -t pablo90ar/activo-frontend:latest .
