fd24bd221669400e6e5f6d47260012c6b7b6cfd1
Pablo Funes <pabafunes@teco.com.ar>
2026-03-03 12:58:32 -0300

    minor fix

--- pi/docker_build_local.sh
+++ pi/docker_build_local.sh
@@ -6,14 +6,14 @@ echo "Building images locally for ARM architecture..."
 
 echo "Building backend..."
 cd back
-docker buildx build --platform linux/arm64 -t pablo90ar/activo-backend:latest . --push
+docker buildx build --platform linux/arm/v8 -t pablo90ar/activo-backend:latest . --push
 
 
 cd ..
 
 echo "Building frontend..."
 cd front
-docker buildx build --platform linux/arm64 -t pablo90ar/activo-frontend:latest . --push
+docker buildx build --platform linux/arm/v8 -t pablo90ar/activo-frontend:latest . --push
 
 
 echo "Local build complete!"
